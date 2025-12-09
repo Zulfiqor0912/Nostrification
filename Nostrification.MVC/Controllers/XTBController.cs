cebed1c256b09915e5e579b8788f9f50502a6bc1
Zulfiqor Rustamov <zulfiqorrustamov81@gmail.com>
2025-12-09 19:53:40 +0500

    e

--- Nostrification.MVC/Controllers/XTBController.cs
+++ Nostrification.MVC/Controllers/XTBController.cs
@@ -2,13 +2,14 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Formatters;
+using Nostrification.Application.Claims.Queries.GetClaimById;
 using Nostrification.Application.Claims.Queries.GetClaims;
 using Nostrification.Application.Users.Queries.GetUserByLogin;
 using Nostrification.MVC.Models.ViewModels;
 
 namespace Nostrification.MVC.Controllers;
 
-//[Authorize(Roles ="XTB")]
+[Authorize(Roles = "Hudud XTB")]
 public class XTBController(IMediator mediator) : BaseController
 {
     public async Task<IActionResult> Index()
@@ -46,5 +47,20 @@ public class XTBController(IMediator mediator) : BaseController
             return View(claims.Where(x => x.CreateDate < DateTime.Now.AddDays(-5) && x.StatusId < 3));
         else
             return View(claims.Where(x => x.StatusId == id));
+    }
+
+    public async Task<IActionResult> ClaimDetails(int id)
+    {
+        var username = User?.Identity?.Name;
+        var user = await mediator.Send(new GetUserByLoginQuery(username));
+        var claim = await mediator.Send(new GetClaimByIdQuery(id));
+
+        if (id == 0 || claim is null || claim.RegionId != user.RegionId) return RedirectToAction("Claims");
+
+        if (claim.StatusId == 1)
+        {
+
+        }
+
     }
 }
